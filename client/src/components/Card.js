72372fe99ac344e3a8ad520b6a076b5ea2626766
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-11-30 10:40:35 -0700

    Remove debug
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,8 +1,6 @@
-// import React from 'react';
 import styled from 'styled-components';
 
 const Card = styled.section`
-    /* background: #300a24; */
     background: ${props => (props.variant === 'light' ? 'white' : '#300a24')};
     color: ${props => (props.variant === 'light' ? '#300a24' : 'white')};
     padding: 1rem;
